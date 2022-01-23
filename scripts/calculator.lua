95a96afcaef21a079140167cc6035d0bdf6ee45c
garfieldcatto <71535863+2dgeneralspam1@users.noreply.github.com>
2022-01-23 10:57:14 -0500

    Create calculator.lua

---
+++ scripts/calculator.lua
@@ -0,0 +1,847 @@
+-- developed by garfield#9172
+
+local function add(a,b)
+	if a == 0 and b == 0 then 
+		return 0 
+	elseif a == 0 and b == 1 then
+		return 1
+	elseif a == 0 and b == 2 then
+		return 2
+	elseif a == 0 and b == 3 then
+		return 3
+	elseif a == 0 and b == 4 then
+		return 4
+	elseif a == 0 and b == 5 then
+		return 5
+	elseif a == 0 and b == 6 then
+		return 6
+	elseif a == 0 and b == 7 then
+		return 7
+	elseif a == 0 and b == 8 then
+		return 8
+	elseif a == 0 and b == 9 then
+		return 9
+	elseif a == 0 and b == 10 then
+		return 10
+	elseif a == 0 and b == 11 then
+		return 11
+	elseif a == 0 and b == 12 then
+		return 12
+	elseif a == 0 and b == 13 then
+		return 13
+	elseif a == 0 and b == 14 then
+		return 14
+	elseif a == 0 and b == 15 then
+		return 15
+	elseif a == 0 and b == 16 then
+		return 16
+	elseif a == 0 and b == 17 then
+		return 17
+	elseif a == 0 and b == 18 then
+		return 18
+	elseif a == 0 and b == 19 then
+		return 19
+	elseif a == 0 and b == 20 then
+		return 20
+	elseif a == 1 and b == 1 then
+		return 2
+	elseif a == 1 and b == 2 then
+		return 3
+	elseif a == 1 and b == 3 then
+		return 4
+	elseif a == 1 and b == 4 then
+		return 5
+	elseif a == 1 and b == 5 then
+		return 6
+	elseif a == 1 and b == 6 then
+		return 7
+	elseif a == 1 and b == 7 then
+		return 8
+	elseif a == 1 and b == 8 then
+		return 9
+	elseif a == 1 and b == 9 then
+		return 10
+	elseif a == 1 and b == 10 then
+		return 11
+	elseif a == 1 and b == 11 then
+		return 12
+	elseif a == 1 and b == 12 then
+		return 13
+	elseif a == 1 and b == 13 then
+		return 14
+	elseif a == 1 and b == 14 then
+		return 15
+	elseif a == 1 and b == 15 then
+		return 16
+	elseif a == 1 and b == 16 then
+		return 17
+	elseif a == 1 and b == 17 then
+		return 18
+	elseif a == 1 and b == 18 then
+		return 19
+	elseif a == 1 and b == 19 then
+		return 20
+	elseif a == 1 and b == 20 then
+		return 21
+	elseif a == 2 and b == 1 then
+		return 3
+	elseif a == 2 and b == 2 then
+		return 4
+	elseif a == 2 and b == 3 then
+		return 5
+	elseif a == 2 and b == 4 then
+		return 6
+	elseif a == 2 and b == 5 then
+		return 7
+	elseif a == 2 and b == 6 then
+		return 8
+	elseif a == 2 and b == 7 then
+		return 9
+	elseif a == 2 and b == 8 then
+		return 10
+	elseif a == 2 and b == 9 then
+		return 11
+	elseif a == 2 and b == 10 then
+		return 12
+	elseif a == 2 and b == 11 then
+		return 13
+	elseif a == 2 and b == 12 then
+		return 14
+	elseif a == 2 and b == 13 then
+		return 15
+	elseif a == 2 and b == 14 then
+		return 16
+	elseif a == 2 and b == 15 then
+		return 17
+	elseif a == 2 and b == 16 then
+		return 18
+	elseif a == 2 and b == 17 then
+		return 19
+	elseif a == 2 and b == 18 then
+		return 20
+	elseif a == 2 and b == 19 then
+		return 21
+	elseif a == 2 and b == 20 then
+		return 22
+	elseif a == 3 and b == 1 then
+		return 4
+	elseif a == 3 and b == 2 then
+		return 5
+	elseif a == 3 and b == 3 then
+		return 6
+	elseif a == 3 and b == 4 then
+		return 7
+	elseif a == 3 and b == 5 then
+		return 8
+	elseif a == 3 and b == 6 then
+		return 9
+	elseif a == 3 and b == 7 then
+		return 10
+	elseif a == 3 and b == 8 then
+		return 11
+	elseif a == 3 and b == 9 then
+		return 12
+	elseif a == 3 and b == 10 then
+		return 13
+	elseif a == 3 and b == 11 then
+		return 14
+	elseif a == 3 and b == 12 then
+		return 15
+	elseif a == 3 and b == 13 then
+		return 16
+	elseif a == 3 and b == 14 then
+		return 17
+	elseif a == 3 and b == 15 then
+		return 18
+	elseif a == 3 and b == 16 then
+		return 19
+	elseif a == 3 and b == 17 then
+		return 20
+	elseif a == 3 and b == 18 then
+		return 21
+	elseif a == 3 and b == 19 then
+		return 22
+	elseif a == 3 and b == 20 then
+		return 23
+	elseif a == 4 and b == 1 then
+		return 5
+	elseif a == 4 and b == 2 then
+		return 6
+	elseif a == 4 and b == 3 then
+		return 7
+	elseif a == 4 and b == 4 then
+		return 8
+	elseif a == 4 and b == 5 then
+		return 9
+	elseif a == 4 and b == 6 then
+		return 10
+	elseif a == 4 and b == 7 then
+		return 11
+	elseif a == 4 and b == 8 then
+		return 12
+	elseif a == 4 and b == 9 then
+		return 13
+	elseif a == 4 and b == 10 then
+		return 14
+	elseif a == 4 and b == 11 then
+		return 15
+	elseif a == 4 and b == 12 then
+		return 16
+	elseif a == 4 and b == 13 then
+		return 17
+	elseif a == 4 and b == 14 then
+		return 18
+	elseif a == 4 and b == 15 then
+		return 19
+	elseif a == 4 and b == 16 then
+		return 20
+	elseif a == 4 and b == 17 then
+		return 21
+	elseif a == 4 and b == 18 then
+		return 22
+	elseif a == 4 and b == 19 then
+		return 23
+	elseif a == 4 and b == 20 then
+		return 24
+	elseif a == 5 and b == 1 then
+		return 6
+	elseif a == 5 and b == 2 then
+		return 7
+	elseif a == 5 and b == 3 then
+		return 8
+	elseif a == 5 and b == 4 then
+		return 9
+	elseif a == 5 and b == 5 then
+		return 10
+	elseif a == 5 and b == 6 then
+		return 11
+	elseif a == 5 and b == 7 then
+		return 12
+	elseif a == 5 and b == 8 then
+		return 13
+	elseif a == 5 and b == 9 then
+		return 14
+	elseif a == 5 and b == 10 then
+		return 15
+	elseif a == 5 and b == 11 then
+		return 16
+	elseif a == 5 and b == 12 then
+		return 17
+	elseif a == 5 and b == 13 then
+		return 18
+	elseif a == 5 and b == 14 then
+		return 19
+	elseif a == 5 and b == 15 then
+		return 20
+	elseif a == 5 and b == 16 then
+		return 21
+	elseif a == 5 and b == 17 then
+		return 22
+	elseif a == 5 and b == 18 then
+		return 23
+	elseif a == 5 and b == 19 then
+		return 24
+	elseif a == 5 and b == 20 then
+		return 25
+	elseif a == 6 and b == 1 then
+		return 7
+	elseif a == 6 and b == 2 then
+		return 8
+	elseif a == 6 and b == 3 then
+		return 9
+	elseif a == 6 and b == 4 then
+		return 10
+	elseif a == 6 and b == 5 then
+		return 11
+	elseif a == 6 and b == 6 then
+		return 12
+	elseif a == 6 and b == 7 then
+		return 13
+	elseif a == 6 and b == 8 then
+		return 14
+	elseif a == 6 and b == 9 then
+		return 15
+	elseif a == 6 and b == 10 then
+		return 16
+	elseif a == 6 and b == 11 then
+		return 17
+	elseif a == 6 and b == 12 then
+		return 18
+	elseif a == 6 and b == 13 then
+		return 19
+	elseif a == 6 and b == 14 then
+		return 20
+	elseif a == 6 and b == 15 then
+		return 21
+	elseif a == 6 and b == 16 then
+		return 22
+	elseif a == 6 and b == 17 then
+		return 23
+	elseif a == 6 and b == 18 then
+		return 24
+	elseif a == 6 and b == 19 then
+		return 25
+	elseif a == 6 and b == 20 then
+		return 26
+	elseif a == 7 and b == 1 then
+		return 8
+	elseif a == 7 and b == 2 then
+		return 9
+	elseif a == 7 and b == 3 then
+		return 10
+	elseif a == 7 and b == 4 then
+		return 11
+	elseif a == 7 and b == 5 then
+		return 12
+	elseif a == 7 and b == 6 then
+		return 13
+	elseif a == 7 and b == 7 then
+		return 14
+	elseif a == 7 and b == 8 then
+		return 15
+	elseif a == 7 and b == 9 then
+		return 16
+	elseif a == 7 and b == 10 then
+		return 17
+	elseif a == 7 and b == 11 then
+		return 18
+	elseif a == 7 and b == 12 then
+		return 19
+	elseif a == 7 and b == 13 then
+		return 20
+	elseif a == 7 and b == 14 then
+		return 21
+	elseif a == 7 and b == 15 then
+		return 22
+	elseif a == 7 and b == 16 then
+		return 23
+	elseif a == 7 and b == 17 then
+		return 24
+	elseif a == 7 and b == 18 then
+		return 25
+	elseif a == 7 and b == 19 then
+		return 26
+	elseif a == 7 and b == 20 then
+		return 27
+	elseif a == 8 and b == 1 then
+		return 9
+	elseif a == 8 and b == 2 then
+		return 10
+	elseif a == 8 and b == 3 then
+		return 11
+	elseif a == 8 and b == 4 then
+		return 12
+	elseif a == 8 and b == 5 then
+		return 13
+	elseif a == 8 and b == 6 then
+		return 14
+	elseif a == 8 and b == 7 then
+		return 15
+	elseif a == 8 and b == 8 then
+		return 16
+	elseif a == 8 and b == 9 then
+		return 17
+	elseif a == 8 and b == 10 then
+		return 18
+	elseif a == 8 and b == 11 then
+		return 19
+	elseif a == 8 and b == 12 then
+		return 20
+	elseif a == 8 and b == 13 then
+		return 21
+	elseif a == 8 and b == 14 then
+		return 22
+	elseif a == 8 and b == 15 then
+		return 23
+	elseif a == 8 and b == 16 then
+		return 24
+	elseif a == 8 and b == 17 then
+		return 25
+	elseif a == 8 and b == 18 then
+		return 26
+	elseif a == 8 and b == 19 then
+		return 27
+	elseif a == 8 and b == 20 then
+		return 28
+	elseif a == 9 and b == 1 then
+		return 10
+	elseif a == 9 and b == 2 then
+		return 11
+	elseif a == 9 and b == 3 then
+		return 12
+	elseif a == 9 and b == 4 then
+		return 13
+	elseif a == 9 and b == 5 then
+		return 14
+	elseif a == 9 and b == 6 then
+		return 15
+	elseif a == 9 and b == 7 then
+		return 16
+	elseif a == 9 and b == 8 then
+		return 17
+	elseif a == 9 and b == 9 then
+		return 18
+	elseif a == 9 and b == 10 then
+		return 19
+	elseif a == 9 and b == 11 then
+		return 20
+	elseif a == 9 and b == 12 then
+		return 21
+	elseif a == 9 and b == 13 then
+		return 22
+	elseif a == 9 and b == 14 then
+		return 23
+	elseif a == 9 and b == 15 then
+		return 24
+	elseif a == 9 and b == 16 then
+		return 25
+	elseif a == 9 and b == 17 then
+		return 26
+	elseif a == 9 and b == 18 then
+		return 27
+	elseif a == 9 and b == 19 then
+		return 28
+	elseif a == 9 and b == 20 then
+		return 29
+	elseif a == 10 and b == 1 then
+		return 11
+	elseif a == 10 and b == 2 then
+		return 12
+	elseif a == 10 and b == 3 then
+		return 13
+	elseif a == 10 and b == 4 then
+		return 14
+	elseif a == 10 and b == 5 then
+		return 15
+	elseif a == 10 and b == 6 then
+		return 16
+	elseif a == 10 and b == 7 then
+		return 17
+	elseif a == 10 and b == 8 then
+		return 18
+	elseif a == 10 and b == 9 then
+		return 19
+	elseif a == 10 and b == 10 then
+		return 20
+	elseif a == 10 and b == 11 then
+		return 21
+	elseif a == 10 and b == 12 then
+		return 22
+	elseif a == 10 and b == 13 then
+		return 23
+	elseif a == 10 and b == 14 then
+		return 24
+	elseif a == 10 and b == 15 then
+		return 25
+	elseif a == 10 and b == 16 then
+		return 26
+	elseif a == 10 and b == 17 then
+		return 27
+	elseif a == 10 and b == 18 then
+		return 28
+	elseif a == 10 and b == 19 then
+		return 29
+	elseif a == 10 and b == 20 then
+		return 30
+	elseif a == 11 and b == 1 then
+		return 12
+	elseif a == 11 and b == 2 then
+		return 13
+	elseif a == 11 and b == 3 then
+		return 14
+	elseif a == 11 and b == 4 then
+		return 15
+	elseif a == 11 and b == 5 then
+		return 16
+	elseif a == 11 and b == 6 then
+		return 17
+	elseif a == 11 and b == 7 then
+		return 18
+	elseif a == 11 and b == 8 then
+		return 19
+	elseif a == 11 and b == 9 then
+		return 20
+	elseif a == 11 and b == 10 then
+		return 21
+	elseif a == 11 and b == 11 then
+		return 22
+	elseif a == 11 and b == 12 then
+		return 23
+	elseif a == 11 and b == 13 then
+		return 24
+	elseif a == 11 and b == 14 then
+		return 25
+	elseif a == 11 and b == 15 then
+		return 26
+	elseif a == 11 and b == 16 then
+		return 27
+	elseif a == 11 and b == 17 then
+		return 28
+	elseif a == 11 and b == 18 then
+		return 29
+	elseif a == 11 and b == 19 then
+		return 30
+	elseif a == 11 and b == 20 then
+		return 31
+	elseif a == 12 and b == 1 then
+		return 13
+	elseif a == 12 and b == 2 then
+		return 14
+	elseif a == 12 and b == 3 then
+		return 15
+	elseif a == 12 and b == 4 then
+		return 16
+	elseif a == 12 and b == 5 then
+		return 17
+	elseif a == 12 and b == 6 then
+		return 18
+	elseif a == 12 and b == 7 then
+		return 19
+	elseif a == 12 and b == 8 then
+		return 20
+	elseif a == 12 and b == 9 then
+		return 21
+	elseif a == 12 and b == 10 then
+		return 22
+	elseif a == 12 and b == 11 then
+		return 23
+	elseif a == 12 and b == 12 then
+		return 24
+	elseif a == 12 and b == 13 then
+		return 25
+	elseif a == 12 and b == 14 then
+		return 26
+	elseif a == 12 and b == 15 then
+		return 27
+	elseif a == 12 and b == 16 then
+		return 28
+	elseif a == 12 and b == 17 then
+		return 29
+	elseif a == 12 and b == 18 then
+		return 30
+	elseif a == 12 and b == 19 then
+		return 31
+	elseif a == 12 and b == 20 then
+		return 32
+	elseif a == 13 and b == 1 then
+		return 14
+	elseif a == 13 and b == 2 then
+		return 15
+	elseif a == 13 and b == 3 then
+		return 16
+	elseif a == 13 and b == 4 then
+		return 17
+	elseif a == 13 and b == 5 then
+		return 18
+	elseif a == 13 and b == 6 then
+		return 19
+	elseif a == 13 and b == 7 then
+		return 20
+	elseif a == 13 and b == 8 then
+		return 21
+	elseif a == 13 and b == 9 then
+		return 22
+	elseif a == 13 and b == 10 then
+		return 23
+	elseif a == 13 and b == 11 then
+		return 24
+	elseif a == 13 and b == 12 then
+		return 25
+	elseif a == 13 and b == 13 then
+		return 26
+	elseif a == 13 and b == 14 then
+		return 27
+	elseif a == 13 and b == 15 then
+		return 28
+	elseif a == 13 and b == 16 then
+		return 29
+	elseif a == 13 and b == 17 then
+		return 30
+	elseif a == 13 and b == 18 then
+		return 31
+	elseif a == 13 and b == 19 then
+		return 32
+	elseif a == 13 and b == 20 then
+		return 33
+	elseif a == 14 and b == 1 then
+		return 15
+	elseif a == 14 and b == 2 then
+		return 16
+	elseif a == 14 and b == 3 then
+		return 17
+	elseif a == 14 and b == 4 then
+		return 18
+	elseif a == 14 and b == 5 then
+		return 19
+	elseif a == 14 and b == 6 then
+		return 20
+	elseif a == 14 and b == 7 then
+		return 21
+	elseif a == 14 and b == 8 then
+		return 22
+	elseif a == 14 and b == 9 then
+		return 23
+	elseif a == 14 and b == 10 then
+		return 24
+	elseif a == 14 and b == 11 then
+		return 25
+	elseif a == 14 and b == 12 then
+		return 26
+	elseif a == 14 and b == 13 then
+		return 27
+	elseif a == 14 and b == 14 then
+		return 28
+	elseif a == 14 and b == 15 then
+		return 29
+	elseif a == 14 and b == 16 then
+		return 30
+	elseif a == 14 and b == 17 then
+		return 31
+	elseif a == 14 and b == 18 then
+		return 32
+	elseif a == 14 and b == 19 then
+		return 33
+	elseif a == 14 and b == 20 then
+		return 34
+	elseif a == 15 and b == 1 then
+		return 16
+	elseif a == 15 and b == 2 then
+		return 17
+	elseif a == 15 and b == 3 then
+		return 18
+	elseif a == 15 and b == 4 then
+		return 19
+	elseif a == 15 and b == 5 then
+		return 20
+	elseif a == 15 and b == 6 then
+		return 21
+	elseif a == 15 and b == 7 then
+		return 22
+	elseif a == 15 and b == 8 then
+		return 23
+	elseif a == 15 and b == 9 then
+		return 24
+	elseif a == 15 and b == 10 then
+		return 25
+	elseif a == 15 and b == 11 then
+		return 26
+	elseif a == 15 and b == 12 then
+		return 27
+	elseif a == 15 and b == 13 then
+		return 28
+	elseif a == 15 and b == 14 then
+		return 29
+	elseif a == 15 and b == 15 then
+		return 30
+	elseif a == 15 and b == 16 then
+		return 31
+	elseif a == 15 and b == 17 then
+		return 32
+	elseif a == 15 and b == 18 then
+		return 33
+	elseif a == 15 and b == 19 then
+		return 34
+	elseif a == 15 and b == 20 then
+		return 35
+	elseif a == 16 and b == 1 then
+		return 17
+	elseif a == 16 and b == 2 then
+		return 18
+	elseif a == 16 and b == 3 then
+		return 19
+	elseif a == 16 and b == 4 then
+		return 20
+	elseif a == 16 and b == 5 then
+		return 21
+	elseif a == 16 and b == 6 then
+		return 22
+	elseif a == 16 and b == 7 then
+		return 23
+	elseif a == 16 and b == 8 then
+		return 24
+	elseif a == 16 and b == 9 then
+		return 25
+	elseif a == 16 and b == 10 then
+		return 26
+	elseif a == 16 and b == 11 then
+		return 27
+	elseif a == 16 and b == 12 then
+		return 28
+	elseif a == 16 and b == 13 then
+		return 29
+	elseif a == 16 and b == 14 then
+		return 30
+	elseif a == 16 and b == 15 then
+		return 31
+	elseif a == 16 and b == 16 then
+		return 32
+	elseif a == 16 and b == 17 then
+		return 33
+	elseif a == 16 and b == 18 then
+		return 34
+	elseif a == 16 and b == 19 then
+		return 35
+	elseif a == 16 and b == 20 then
+		return 36
+	elseif a == 17 and b == 1 then
+		return 18
+	elseif a == 17 and b == 2 then
+		return 19
+	elseif a == 17 and b == 3 then
+		return 20
+	elseif a == 17 and b == 4 then
+		return 21
+	elseif a == 17 and b == 5 then
+		return 22
+	elseif a == 17 and b == 6 then
+		return 23
+	elseif a == 17 and b == 7 then
+		return 24
+	elseif a == 17 and b == 8 then
+		return 25
+	elseif a == 17 and b == 9 then
+		return 26
+	elseif a == 17 and b == 10 then
+		return 27
+	elseif a == 17 and b == 11 then
+		return 28
+	elseif a == 17 and b == 12 then
+		return 29
+	elseif a == 17 and b == 13 then
+		return 30
+	elseif a == 17 and b == 14 then
+		return 31
+	elseif a == 17 and b == 15 then
+		return 32
+	elseif a == 17 and b == 16 then
+		return 33
+	elseif a == 17 and b == 17 then
+		return 34
+	elseif a == 17 and b == 18 then
+		return 35
+	elseif a == 17 and b == 19 then
+		return 36
+	elseif a == 17 and b == 20 then
+		return 37
+	elseif a == 18 and b == 1 then
+		return 19
+	elseif a == 18 and b == 2 then
+		return 20
+	elseif a == 18 and b == 3 then
+		return 21
+	elseif a == 18 and b == 4 then
+		return 22
+	elseif a == 18 and b == 5 then
+		return 23
+	elseif a == 18 and b == 6 then
+		return 24
+	elseif a == 18 and b == 7 then
+		return 25
+	elseif a == 18 and b == 8 then
+		return 26
+	elseif a == 18 and b == 9 then
+		return 27
+	elseif a == 18 and b == 10 then
+		return 28
+	elseif a == 18 and b == 11 then
+		return 29
+	elseif a == 18 and b == 12 then
+		return 30
+	elseif a == 18 and b == 13 then
+		return 31
+	elseif a == 18 and b == 14 then
+		return 32
+	elseif a == 18 and b == 15 then
+		return 33
+	elseif a == 18 and b == 16 then
+		return 34
+	elseif a == 18 and b == 17 then
+		return 35
+	elseif a == 18 and b == 18 then
+		return 36
+	elseif a == 18 and b == 19 then
+		return 37
+	elseif a == 18 and b == 20 then
+		return 38
+	elseif a == 19 and b == 1 then
+		return 20
+	elseif a == 19 and b == 2 then
+		return 21
+	elseif a == 19 and b == 3 then
+		return 22
+	elseif a == 19 and b == 4 then
+		return 23
+	elseif a == 19 and b == 5 then
+		return 24
+	elseif a == 19 and b == 6 then
+		return 25
+	elseif a == 19 and b == 7 then
+		return 26
+	elseif a == 19 and b == 8 then
+		return 27
+	elseif a == 19 and b == 9 then
+		return 28
+	elseif a == 19 and b == 10 then
+		return 29
+	elseif a == 19 and b == 11 then
+		return 30
+	elseif a == 19 and b == 12 then
+		return 31
+	elseif a == 19 and b == 13 then
+		return 32
+	elseif a == 19 and b == 14 then
+		return 33
+	elseif a == 19 and b == 15 then
+		return 34
+	elseif a == 19 and b == 16 then
+		return 35
+	elseif a == 19 and b == 17 then
+		return 36
+	elseif a == 19 and b == 18 then
+		return 37
+	elseif a == 19 and b == 19 then
+		return 38
+	elseif a == 19 and b == 20 then
+		return 39
+	elseif a == 20 and b == 1 then
+		return 21
+	elseif a == 20 and b == 2 then
+		return 22
+	elseif a == 20 and b == 3 then
+		return 23
+	elseif a == 20 and b == 4 then
+		return 24
+	elseif a == 20 and b == 5 then
+		return 25
+	elseif a == 20 and b == 6 then
+		return 26
+	elseif a == 20 and b == 7 then
+		return 27
+	elseif a == 20 and b == 8 then
+		return 28
+	elseif a == 20 and b == 9 then
+		return 29
+	elseif a == 20 and b == 10 then
+		return 30
+	elseif a == 20 and b == 11 then
+		return 31
+	elseif a == 20 and b == 12 then
+		return 32
+	elseif a == 20 and b == 13 then
+		return 33
+	elseif a == 20 and b == 14 then
+		return 34
+	elseif a == 20 and b == 15 then
+		return 35
+	elseif a == 20 and b == 16 then
+		return 36
+	elseif a == 20 and b == 17 then
+		return 37
+	elseif a == 20 and b == 18 then
+		return 38
+	elseif a == 20 and b == 19 then
+		return 39
+	elseif a == 20 and b == 20 then
+		return 40
+	end
+end
