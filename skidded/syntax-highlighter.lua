9ba1cef935a1ee307d1ca03a97df7cef89f86e5d
garfieldcatto <71535863+2dgeneralspam1@users.noreply.github.com>
2022-01-29 11:40:04 -0500

    updated to the lastest
    from here https://raw.githubusercontent.com/NotDSF/HttpSpy/main/init.lua

--- skidded/syntax-highlighter.lua
+++ skidded/syntax-highlighter.lua
@@ -1,30 +1,21 @@
--- This version is built for the roblox platform.
--- https://raw.githubusercontent.com/NotDSF/Lua-Serializer/main/Serializer%20Highlighting.lua
-
-local format   = string.format;
-local rep      = string.rep;
-local Type     = type;
+local config   = { spaces = 4, highlighting = false };
+local str      = string;
+local gme      = game;
+local sub      = str.sub;
+local format   = str.format;
+local rep      = str.rep;
+local byte     = str.byte;
+local match    = str.match;
+local getfn    = gme.GetFullName;
+local info     = debug.getinfo;
+local huge     = math.huge; -- just like your mother
+local Type     = typeof;
 local Pairs    = pairs;
-local gsub     = string.gsub;
-local sub      = string.sub;
+local Assert   = assert;
 local Tostring = tostring;
 local concat   = table.concat;
-local Tab      = rep(" ", 4);
-
+local Tab      = rep(" ", config.spaces or 4);
 local Serialize;
-local function formatIndex(idx, scope)
-  local indexType = Type(idx);
-  local finishedFormat = idx;
-  if indexType == "string" then
-    finishedFormat = format("\27[32m\"%s\"\27[0m", idx); 
-  elseif indexType == "table" then
-    scope = scope + 1;
-    finishedFormat = Serialize(idx, scope);
-  elseif indexType == "number" then
-    finishedFormat = format("\27[33m%s\27[0m", idx);
-  end;
-  return format("[%s]", finishedFormat);
-end;
 
 local function serializeArgs(...) 
   local Serialized = {}; -- For performance reasons
@@ -33,7 +24,7 @@ local function serializeArgs(...)
     local valueType = Type(v);
     local SerializeIndex = #Serialized + 1;
     if valueType == "string" then
-      Serialized[SerializeIndex] = format("\27[32m\"%s\"\27[0m", v);
+      Serialized[SerializeIndex] = format("\"%s\"", v);
     elseif valueType == "table" then
       Serialized[SerializeIndex] = Serialize(v, 0);
     else
@@ -44,13 +35,87 @@ local function serializeArgs(...)
   return concat(Serialized, ", ");
 end;
 
--- Very scuffed method I know
+local function formatFunction(func)
+  if info then -- Creates function prototypes
+    local proto = info(func);
+    local params = {};
+
+    if proto.nparams then
+      for i=1, proto.nparams do
+        params[i] = format("p%d", i);
+      end;
+      if proto.isvararg then
+        params[#params+1] = "...";
+      end;
+    end;
+
+    return format("function (%s) --[[ Function Name: \"%s\" ]] end", concat(params, ", "), proto.namewhat or proto.name or "");
+  end;
+  return "function () end"; -- we cannot create a prototype
+end;
 
 local function formatString(str) 
-  for i,v in Pairs({ ["\n"] = "\\n", ["\t"] = "\\t", ["\""] = "\\\"" }) do
-    str = gsub(str, i, v);
+  local Pos = 1;
+  local String = {};
+  while Pos <= #str do
+    local Key = sub(str, Pos, Pos);
+    if Key == "\n" then
+      String[Pos] = "\\n";
+    elseif Key == "\t" then
+      String[Pos] = "\\t";
+    elseif Key == "\"" then
+      String[Pos] = "\\\"";
+    else
+      local Code = byte(Key);
+      if Code < 32 or Code > 126 then
+        String[Pos] = format("\\%d", Code);
+      else
+        String[Pos] = Key;
+      end;
+    end;
+    Pos = Pos + 1;
   end;
-  return str;
+  return concat(String);
+end;
+
+-- We can do a little trolling and use this for booleans too
+local function formatNumber(numb) 
+  if numb == huge then
+    return "math.huge";
+  elseif numb == -huge then
+    return "-math.huge";
+  end;
+  return Tostring(numb);
+end;
+
+local function formatIndex(idx, scope)
+  local indexType = Type(idx);
+  local finishedFormat = idx;
+
+  if indexType == "string" then
+    if match(idx, "[^_%a%d]+") then
+      finishedFormat = format(config.highlighting and "\27[32m\"%s\"\27[0m" or "\"%s\"", formatString(idx));
+    else
+      return idx;
+    end;
+  elseif indexType == "table" then
+    scope = scope + 1;
+    finishedFormat = Serialize(idx, scope);
+  elseif indexType == "number" or indexType == "boolean" then
+    if config.highlighting then
+      finishedFormat = format("\27[33m%s\27[0m", formatNumber(idx));
+    else
+      finishedFormat = formatNumber(idx);
+    end;
+  elseif indexType == "function" then
+    finishedFormat = formatFunction(idx);
+  elseif indexType == "Instance" then
+    finishedFormat = getfn(idx);
+  else
+    finishedFormat = Tostring(idx);
+  end;
+
+  return format("[%s]", finishedFormat);
 end;
 
 Serialize = function(tbl, scope) 
@@ -66,15 +131,21 @@ Serialize = function(tbl, scope)
     local valueType = Type(v);
     local SerializeIndex = #Serialized + 1;
     if valueType == "string" then -- Could of made it inline but its better to manage types this way.
-      Serialized[SerializeIndex] = format("%s%s = \27[32m\"%s\"\27[0m,\n", scopeTab2, formattedIndex, formatString(v));
+      Serialized[SerializeIndex] = format(config.highlighting and "%s%s = \27[32m\"%s\"\27[0m,\n" or "%s%s = \"%s\",\n", scopeTab2, formattedIndex, formatString(v));
     elseif valueType == "number" or valueType == "boolean" then
-      Serialized[SerializeIndex] = format("%s%s = \27[33m%s\27[0m,\n", scopeTab2, formattedIndex, Tostring(v));
+      Serialized[SerializeIndex] = format(config.highlighting and "%s%s = \27[33m%s\27[0m,\n" or "%s%s = %s,\n", scopeTab2, formattedIndex, formatNumber(v));
     elseif valueType == "table" then
       Serialized[SerializeIndex] = format("%s%s = %s,\n", scopeTab2, formattedIndex, Serialize(v, scope+1));
     elseif valueType == "userdata" then
       Serialized[SerializeIndex] = format("%s%s = newproxy(),\n", scopeTab2, formattedIndex);
+    elseif valueType == "function" then
+      Serialized[SerializeIndex] = format("%s%s = %s,\n", scopeTab2, formattedIndex, formatFunction(v));
+    elseif valueType == "Instance" then
+      Serialized[SerializeIndex] = format("%s%s = %s,\n", scopeTab2, formattedIndex, getfn(v));
+    elseif valueType == "CFrame" or valueType == "Vector3" or valueType == "Vector2" or valueType == "UDim2" then
+      Serialized[SerializeIndex] = format("%s%s = %s.new(%s),\n", scopeTab2, formattedIndex, valueType, Tostring(v));
     else
-      Serialized[SerializeIndex] = format("%s%s = \"%s\",\n", scopeTab2, formattedIndex, Tostring(valueType)); -- Unsupported types.
+      Serialized[SerializeIndex] = format("%s%s = \"%s\",\n", scopeTab2, formattedIndex, Tostring(v)); -- Unsupported types.
     end;
     tblLen = tblLen + 1; -- # messes up with nil values
   end;
@@ -96,15 +167,26 @@ Serialize = function(tbl, scope)
   end;
 end;
 
-local SerializeL = {
-  formatIndex = formatIndex,
-  formatString = formatString,
-  serializeArgs = serializeArgs,
-  config = config
-}
+local Serializer = {};
 
-return setmetatable(SerializeL, {
-  __call = function(self, ...)
-    return Serialize(...);
-  end
-});
+function Serializer.Serialize(tbl)
+  Assert(Type(tbl) == "table", "invalid argument #1 to 'Serialize' (table expected)");
+  return Serialize(tbl);
+end;
+
+function Serializer.FormatArguments(...) 
+  return serializeArgs(...);
+end;
+
+function Serializer.FormatString(str) 
+  Assert(Type(str) == "string", "invalid argument #1 to 'FormatString' (string expected)");
+  return formatString(str);
+end;
+
+function Serializer.UpdateConfig(options) 
+  Assert(Type(options) == "table", "invalid argument #1 to 'UpdateConfig' (table expected)");
+  config.spaces = options.spaces or 4;
+  config.highlighting = options.highlighting;
+end;
+
+return Serializer;
