dc89b766cb1f4ae9406b399f7cdd7a5f25c1c99e
garfieldcatto <71535863+2dgeneralspam1@users.noreply.github.com>
2022-04-24 13:16:58 -0400

    Update foppa.lua

--- scripts/foppa.lua
+++ scripts/foppa.lua
@@ -1,3 +1,5 @@
+firstgate = false 
+game.Players.LocalPlayer.DisplayName = "User"
 local function fireproximityprompt(Obj, Amount, Skip)
     if Obj.ClassName == "ProximityPrompt" then 
         Amount = Amount or 1
@@ -41,6 +43,16 @@ local main = Window:MakeTab({
 main:AddLabel("Automation")
 
 main:AddButton({
+    Name = "Save game",
+    Callback = function()
+        local currentpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-81.27365112304688, 7.799998760223389, -24.59555435180664); lookDown(); task.wait(0.3)
+        fireproximityprompt(game:GetService("Workspace")["Floppy Disk"].ProximityPrompt,1,true)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = currentpos
+    end 
+})
+
+main:AddButton({
     Name = "Collect all money",
     Callback = function()
         for i,v in pairs(game.Workspace:GetDescendants()) do
@@ -59,27 +71,22 @@ main:AddButton({
             
             for i,v in pairs(game.Workspace:GetDescendants()) do
                 if v.Name == "Mushroom" and v:IsA("MeshPart") then 
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame; task.wait(0.3); lookDown()
+                    lookDown(); game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame; task.wait(0.3)
                     fireproximityprompt(v.ForagePrompt,1,true); task.wait(0.3)
                 end 
             end
         else
             OrionLib:MakeNotification({
                 Name = "flop",
-                Content = "no mush room",
+                Content = "outdoor not unlocked yet",
+                Image = "rbxassetid://6031071053",
                 Time = 5
             })
         end 
     end
 })
 
-main:AddButton({
-    Name = "Offer Altar",
-    Callback = function()
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Altar["Floppa Cube"].Ears.CFrame;task.wait(0.3)
-        fireproximityprompt(game:GetService("Workspace").Altar["Thin Wall"].ProximityPrompt,1,true)
-    end
-})
+
 
 main:AddToggle({
 	Name = "Auto press floppa",
@@ -89,13 +96,30 @@ main:AddToggle({
 	end    
 })
 
-main:AddToggle({
+local moneyListener; main:AddToggle({
     Name = "Auto collect money",   
     Default = false,
     Callback = function(value)
-        shared.autocollectmoney = value
+        if firstgate then 
+            if value then 
+                for i,v in pairs(game.Workspace:GetDescendants()) do
+                    if v.Name == "Money" and v:IsA("MeshPart") then 
+                        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,v,0)
+                        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,v,1)
+                    end 
+                end; moneyListener = game.Workspace.ChildAdded:Connect(function(Obj)
+                    if Obj.Name == "Money" and Obj:IsA("MeshPart") then 
+                        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,Obj,0)
+                        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,Obj,1)
+                    end 
+                end)
+            else
+                moneyListener:Disconnect()
+            end 
+        end 
     end
 })
+
 
 main:AddLabel("Shops / Teleports")
 
@@ -131,25 +155,168 @@ main:AddButton({
     end
 })
 
+main:AddLabel("Misc")
+
+main:AddButton({
+    Name = "Remove accended obby",
+    Callback = function()
+        if game.Workspace:FindFirstChild("Temple",true) then 
+            game:GetService("Workspace").Temple.Obby:Destroy()
+        else
+            OrionLib:MakeNotification({
+                Name = "flop",
+                Content = "no obby found (did you accend yet?)",
+                Image = "rbxassetid://6031071053",
+                Time = 5
+            })
+        end    
+    end
+})
+
+main:AddButton({
+    Name = "Max Altar (uses all cash)",
+    Callback = function()
+        if game.Workspace:FindFirstChild("Altar") then 
+            OrionLib:MakeNotification({
+                Name = "flop",
+                Content = "setting up altar...",
+                Image = "rbxassetid://6031075926",
+                Time = 5
+            }); game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Altar["Floppa Cube"].Ears.CFrame; task.wait(0.3); lookDown(); task.wait(0.3)
+            local thing = 0; while thing < 50 do 
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Altar["Floppa Cube"].Ears.CFrame; lookDown()
+                fireproximityprompt(game:GetService("Workspace").Altar["Thin Wall"].ProximityPrompt,1,true)
+                thing = thing +1 
+            end; OrionLib:MakeNotification({
+                Name = "flop",
+                Content = "finished offering",
+                Image = "rbxassetid://6031075926",
+                Time = 5
+            }); task.wait(3); OrionLib:MakeNotification({
+                Name = "flop",
+                Content = "done!",
+                Image = "rbxassetid://6023426945",
+                Time = 5
+            })
+        else
+            OrionLib:MakeNotification({
+                Name = "flop",
+                Content = "no altar",
+                Image = "rbxassetid://6031071053",
+                Time = 5
+            })
+        end 
+    end
+})
+
+local almond; local moneyrequired; main:AddSlider({
+	Name = "Almond Water Amount",
+	Min = 1,
+	Max = 25,
+	Default = 1,
+	Increment = 1,
+	ValueName = "Almond Wilk Cartons",
+	Callback = function(Value)
+		almond = Value; moneyrequired = Value * 1000 
+	end    
+})
+
+main:AddButton({
+    Name = "Buy Almond Water",
+    Callback = function()
+        if tonumber(game.Players.LocalPlayer.leaderstats.Money.Value) < moneyrequired then 
+            OrionLib:MakeNotification({
+                Name = "flop",
+                Content = "not enough money (oops!)",
+                Image = "rbxassetid://6031071053",
+                Time = 5
+            })
+        else
+            lookDown()
+            local counter=  0 
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Backrooms.PC.CFrame; task.wait(0.3)
+            fireproximityprompt(game:GetService("Workspace").Backrooms.PC.ProximityPrompt,1,true); task.wait(0.3)
+            while counter ~= almond do 
+                local ohString1 = "Almond Water"
+                game:GetService("ReplicatedStorage").Purchase2:FireServer(ohString1)
+                counter = counter + 1
+            end; game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").HomeTeleport.CFrame
+        end 
+    end
+})
+
+local playertab = Window:MakeTab({
+    Name = "Player",
+    Icon = "rbxassetid://6031289449",
+    PremiumOnly = false
+})
+
+local playerspeed
+local playerjumpspeed 
+playertab:AddSlider({
+	Name = "Walkspeed",
+	Min = 16,
+	Max = 255,
+	Default =16,
+	Increment = 0.5,
+	ValueName = "Studs Per Second",
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+        playerspeed = Value
+	end    
+})
+
+playertab:AddSlider({
+    Name = "JumpPower",
+    Min = 50,
+    Max = 255,
+    Default =50,
+    Increment = 0.5,
+    ValueName = "Studs Per Second",
+    Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+        playerjumpspeed = Value
+    end    
+})
+
+playertab:AddToggle({
+    Name = "Keep values",
+    Default = false,
+    Callback = function(value)
+        shared.keepvalues = value
+    end
+})
+
+local misctab = Window:MakeTab({
+    Name = "Misc",
+    Icon = "rbxassetid://6034509993",
+    PremiumOnly = false
+})
+
+misctab:AddParagraph("credits",[[garfield - scripting\nsowd guy - the proximity prtomnt]])
+misctab:AddButton({
+    Name = "Join Discord",
+    Callback = function()
+        setclipboard("dsc.gg/garfieldwarez")
+        OrionLib:MakeNotification({
+            Name = "flop",
+            Content = "paste link that was copied to ur clip board into ur browser to join !!",
+            Image = "rbxassetid://6034457067",
+            Time = 5
+        })
+    end 
+})
 spawn(function()
     while wait(0.1) do 
         if shared.floppapress then 
             fireclickdetector(game:GetService("Workspace").Floppa.ClickDetector)
         end
+
+        if shared.keepvalues then 
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = playerspeed
+            game.Players.LocalPlayer.Character.Humanoid.JumpPower = playerjumpspeed
+        end
     end
 end)
 
-spawn(function()
-    pcall(function()
-        while task.wait(2) do 
-            if shared.autocollectmoney then 
-                for i,v in pairs(game.Workspace:GetDescendants()) do
-                    if v.Name == "Money" and v:IsA("MeshPart") then 
-                        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,v,0)
-                        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,v,1)
-                    end 
-                end
-            end 
-        end 
-    end) 
-end) 
+firstgate = true 
